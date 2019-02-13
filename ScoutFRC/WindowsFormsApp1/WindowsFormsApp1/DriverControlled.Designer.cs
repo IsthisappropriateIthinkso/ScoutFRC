2b625074c1c0a4344c2f9b7f0886f059cfa01921
IsthisappropriateIthinkso <ag137220@students.davenport.k12.ia.us>
2019-02-13 11:35:02 -0600

    ScoutFRC

---
+++ ScoutFRC/WindowsFormsApp1/WindowsFormsApp1/DriverControlled.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class DriverControlled
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rBtnLow = new System.Windows.Forms.RadioButton();
+            this.rBtnMedium = new System.Windows.Forms.RadioButton();
+            this.rBtnHigh = new System.Windows.Forms.RadioButton();
+            this.tbAttempts_1 = new System.Windows.Forms.TextBox();
+            this.tbDelivered_1 = new System.Windows.Forms.TextBox();
+            this.BtnAttempted_1 = new System.Windows.Forms.Button();
+            this.btnDelivered_1 = new System.Windows.Forms.Button();
+            this.BtnDelivered_2 = new System.Windows.Forms.Button();
+            this.BtnAttempted_2 = new System.Windows.Forms.Button();
+            this.tbDelivered_2 = new System.Windows.Forms.TextBox();
+            this.tbAttempts_2 = new System.Windows.Forms.TextBox();
+            this.rBtnHigh_2 = new System.Windows.Forms.RadioButton();
+            this.rBtnMedium_2 = new System.Windows.Forms.RadioButton();
+            this.rBtnLow_2 = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // rBtnLow
+            // 
+            this.rBtnLow.AutoSize = true;
+            this.rBtnLow.Location = new System.Drawing.Point(448, 107);
+            this.rBtnLow.Name = "rBtnLow";
+            this.rBtnLow.Size = new System.Drawing.Size(45, 17);
+            this.rBtnLow.TabIndex = 0;
+            this.rBtnLow.TabStop = true;
+            this.rBtnLow.Text = "Low";
+            this.rBtnLow.UseVisualStyleBackColor = true;
+            // 
+            // rBtnMedium
+            // 
+            this.rBtnMedium.AutoSize = true;
+            this.rBtnMedium.Location = new System.Drawing.Point(448, 130);
+            this.rBtnMedium.Name = "rBtnMedium";
+            this.rBtnMedium.Size = new System.Drawing.Size(62, 17);
+            this.rBtnMedium.TabIndex = 1;
+            this.rBtnMedium.TabStop = true;
+            this.rBtnMedium.Text = "Medium";
+            this.rBtnMedium.UseVisualStyleBackColor = true;
+            // 
+            // rBtnHigh
+            // 
+            this.rBtnHigh.AutoSize = true;
+            this.rBtnHigh.Location = new System.Drawing.Point(448, 153);
+            this.rBtnHigh.Name = "rBtnHigh";
+            this.rBtnHigh.Size = new System.Drawing.Size(47, 17);
+            this.rBtnHigh.TabIndex = 2;
+            this.rBtnHigh.TabStop = true;
+            this.rBtnHigh.Text = "High";
+            this.rBtnHigh.UseVisualStyleBackColor = true;
+            // 
+            // tbAttempts_1
+            // 
+            this.tbAttempts_1.Location = new System.Drawing.Point(566, 163);
+            this.tbAttempts_1.Name = "tbAttempts_1";
+            this.tbAttempts_1.Size = new System.Drawing.Size(37, 20);
+            this.tbAttempts_1.TabIndex = 3;
+            // 
+            // tbDelivered_1
+            // 
+            this.tbDelivered_1.Location = new System.Drawing.Point(666, 162);
+            this.tbDelivered_1.Name = "tbDelivered_1";
+            this.tbDelivered_1.Size = new System.Drawing.Size(37, 20);
+            this.tbDelivered_1.TabIndex = 6;
+            // 
+            // BtnAttempted_1
+            // 
+            this.BtnAttempted_1.Location = new System.Drawing.Point(543, 124);
+            this.BtnAttempted_1.Name = "BtnAttempted_1";
+            this.BtnAttempted_1.Size = new System.Drawing.Size(75, 33);
+            this.BtnAttempted_1.TabIndex = 14;
+            this.BtnAttempted_1.Text = "Attempted";
+            this.BtnAttempted_1.UseVisualStyleBackColor = true;
+            // 
+            // btnDelivered_1
+            // 
+            this.btnDelivered_1.Location = new System.Drawing.Point(643, 123);
+            this.btnDelivered_1.Name = "btnDelivered_1";
+            this.btnDelivered_1.Size = new System.Drawing.Size(75, 33);
+            this.btnDelivered_1.TabIndex = 15;
+            this.btnDelivered_1.Text = "Delivered";
+            this.btnDelivered_1.UseVisualStyleBackColor = true;
+            // 
+            // BtnDelivered_2
+            // 
+            this.BtnDelivered_2.Location = new System.Drawing.Point(628, 413);
+            this.BtnDelivered_2.Name = "BtnDelivered_2";
+            this.BtnDelivered_2.Size = new System.Drawing.Size(75, 23);
+            this.BtnDelivered_2.TabIndex = 22;
+            this.BtnDelivered_2.Text = "Delivered";
+            this.BtnDelivered_2.UseVisualStyleBackColor = true;
+            // 
+            // BtnAttempted_2
+            // 
+            this.BtnAttempted_2.Location = new System.Drawing.Point(528, 414);
+            this.BtnAttempted_2.Name = "BtnAttempted_2";
+            this.BtnAttempted_2.Size = new System.Drawing.Size(75, 23);
+            this.BtnAttempted_2.TabIndex = 21;
+            this.BtnAttempted_2.Text = "Attempted";
+            this.BtnAttempted_2.UseVisualStyleBackColor = true;
+            // 
+            // tbDelivered_2
+            // 
+            this.tbDelivered_2.Location = new System.Drawing.Point(646, 442);
+            this.tbDelivered_2.Name = "tbDelivered_2";
+            this.tbDelivered_2.Size = new System.Drawing.Size(37, 20);
+            this.tbDelivered_2.TabIndex = 20;
+            // 
+            // tbAttempts_2
+            // 
+            this.tbAttempts_2.Location = new System.Drawing.Point(543, 442);
+            this.tbAttempts_2.Name = "tbAttempts_2";
+            this.tbAttempts_2.Size = new System.Drawing.Size(37, 20);
+            this.tbAttempts_2.TabIndex = 19;
+            // 
+            // rBtnHigh_2
+            // 
+            this.rBtnHigh_2.AutoSize = true;
+            this.rBtnHigh_2.Location = new System.Drawing.Point(433, 443);
+            this.rBtnHigh_2.Name = "rBtnHigh_2";
+            this.rBtnHigh_2.Size = new System.Drawing.Size(47, 17);
+            this.rBtnHigh_2.TabIndex = 18;
+            this.rBtnHigh_2.TabStop = true;
+            this.rBtnHigh_2.Text = "High";
+            this.rBtnHigh_2.UseVisualStyleBackColor = true;
+            // 
+            // rBtnMedium_2
+            // 
+            this.rBtnMedium_2.AutoSize = true;
+            this.rBtnMedium_2.Location = new System.Drawing.Point(433, 420);
+            this.rBtnMedium_2.Name = "rBtnMedium_2";
+            this.rBtnMedium_2.Size = new System.Drawing.Size(62, 17);
+            this.rBtnMedium_2.TabIndex = 17;
+            this.rBtnMedium_2.TabStop = true;
+            this.rBtnMedium_2.Text = "Medium";
+            this.rBtnMedium_2.UseVisualStyleBackColor = true;
+            // 
+            // rBtnLow_2
+            // 
+            this.rBtnLow_2.AutoSize = true;
+            this.rBtnLow_2.Location = new System.Drawing.Point(433, 397);
+            this.rBtnLow_2.Name = "rBtnLow_2";
+            this.rBtnLow_2.Size = new System.Drawing.Size(45, 17);
+            this.rBtnLow_2.TabIndex = 16;
+            this.rBtnLow_2.TabStop = true;
+            this.rBtnLow_2.Text = "Low";
+            this.rBtnLow_2.UseVisualStyleBackColor = true;
+            // 
+            // DriverControlled
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::WindowsFormsApp1.Properties.Resources.FRC_Field;
+            this.ClientSize = new System.Drawing.Size(800, 618);
+            this.Controls.Add(this.BtnDelivered_2);
+            this.Controls.Add(this.BtnAttempted_2);
+            this.Controls.Add(this.tbDelivered_2);
+            this.Controls.Add(this.tbAttempts_2);
+            this.Controls.Add(this.rBtnHigh_2);
+            this.Controls.Add(this.rBtnMedium_2);
+            this.Controls.Add(this.rBtnLow_2);
+            this.Controls.Add(this.btnDelivered_1);
+            this.Controls.Add(this.BtnAttempted_1);
+            this.Controls.Add(this.tbDelivered_1);
+            this.Controls.Add(this.tbAttempts_1);
+            this.Controls.Add(this.rBtnHigh);
+            this.Controls.Add(this.rBtnMedium);
+            this.Controls.Add(this.rBtnLow);
+            this.Name = "DriverControlled";
+            this.Text = "DriverControlled";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton rBtnLow;
+        private System.Windows.Forms.RadioButton rBtnMedium;
+        private System.Windows.Forms.RadioButton rBtnHigh;
+        private System.Windows.Forms.TextBox tbAttempts_1;
+        private System.Windows.Forms.TextBox tbDelivered_1;
+        private System.Windows.Forms.Button BtnAttempted_1;
+        private System.Windows.Forms.Button btnDelivered_1;
+        private System.Windows.Forms.Button BtnDelivered_2;
+        private System.Windows.Forms.Button BtnAttempted_2;
+        private System.Windows.Forms.TextBox tbDelivered_2;
+        private System.Windows.Forms.TextBox tbAttempts_2;
+        private System.Windows.Forms.RadioButton rBtnHigh_2;
+        private System.Windows.Forms.RadioButton rBtnMedium_2;
+        private System.Windows.Forms.RadioButton rBtnLow_2;
+    }
+}
---
+++ ScoutFRC/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbID = new System.Windows.Forms.TextBox();
+            this.tbEvent = new System.Windows.Forms.TextBox();
+            this.tbMatchNum = new System.Windows.Forms.TextBox();
+            this.tbTeamNum = new System.Windows.Forms.TextBox();
+            this.tb = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.BtnContinue = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbID
+            // 
+            this.tbID.Location = new System.Drawing.Point(71, 107);
+            this.tbID.Name = "tbID";
+            this.tbID.Size = new System.Drawing.Size(100, 20);
+            this.tbID.TabIndex = 0;
+            // 
+            // tbEvent
+            // 
+            this.tbEvent.Location = new System.Drawing.Point(201, 107);
+            this.tbEvent.Name = "tbEvent";
+            this.tbEvent.Size = new System.Drawing.Size(100, 20);
+            this.tbEvent.TabIndex = 1;
+            // 
+            // tbMatchNum
+            // 
+            this.tbMatchNum.Location = new System.Drawing.Point(336, 107);
+            this.tbMatchNum.Name = "tbMatchNum";
+            this.tbMatchNum.Size = new System.Drawing.Size(100, 20);
+            this.tbMatchNum.TabIndex = 2;
+            // 
+            // tbTeamNum
+            // 
+            this.tbTeamNum.Location = new System.Drawing.Point(472, 107);
+            this.tbTeamNum.Name = "tbTeamNum";
+            this.tbTeamNum.Size = new System.Drawing.Size(100, 20);
+            this.tbTeamNum.TabIndex = 3;
+            // 
+            // tb
+            // 
+            this.tb.Location = new System.Drawing.Point(596, 107);
+            this.tb.Name = "tb";
+            this.tb.Size = new System.Drawing.Size(100, 20);
+            this.tb.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(91, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Record ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(226, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Event ID";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(348, 71);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Match Number";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(506, 71);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Team ID";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(628, 71);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Alliance ID";
+            // 
+            // BtnContinue
+            // 
+            this.BtnContinue.Location = new System.Drawing.Point(657, 379);
+            this.BtnContinue.Name = "BtnContinue";
+            this.BtnContinue.Size = new System.Drawing.Size(107, 60);
+            this.BtnContinue.TabIndex = 10;
+            this.BtnContinue.Text = "CONTINUE";
+            this.BtnContinue.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(925, 584);
+            this.Controls.Add(this.BtnContinue);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tb);
+            this.Controls.Add(this.tbTeamNum);
+            this.Controls.Add(this.tbMatchNum);
+            this.Controls.Add(this.tbEvent);
+            this.Controls.Add(this.tbID);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbID;
+        private System.Windows.Forms.TextBox tbEvent;
+        private System.Windows.Forms.TextBox tbMatchNum;
+        private System.Windows.Forms.TextBox tbTeamNum;
+        private System.Windows.Forms.TextBox tb;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button BtnContinue;
+    }
+}
+
