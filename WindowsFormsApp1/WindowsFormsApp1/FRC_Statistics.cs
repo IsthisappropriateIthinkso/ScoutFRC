55a28a1ddec25d263b1b031f2b266694cf94d7ad
IsthisappropriateIthinkso <ag137220@students.davenport.k12.ia.us>
2019-02-21 09:45:43 -0600

    Counting Number Of Clicks with Button

---
+++ WindowsFormsApp1/WindowsFormsApp1/FRC_Statistics.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class FRC_Statistics : Form
+    {
+        public FRC_Statistics()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnNextMatch_Click(object sender, EventArgs e)
+        {
+            frmRecordLog form1 = new frmRecordLog();
+            this.Close();
+            form1.Visible=true;
+        }
+    }
+}
